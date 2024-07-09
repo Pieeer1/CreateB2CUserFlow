a82edd9552a7e8c618e43667f70365bce6f01c29
Mason Adams <mason.adams@msn.com>
2024-07-09 11:25:28 -0700

    temp log

--- index.js
+++ index.js
@@ -59,6 +59,7 @@ const bootstrap = async () => {
         });
     }
     catch(error){
+        console.log(error); // temp REMOVE
         if(error.res.statusCode !== 404){
             if(logLevel == 'verbose'){
                 console.error(error);
